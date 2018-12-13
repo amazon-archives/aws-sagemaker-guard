989aded3cd37414554bdba7f4f9e5314f461248c
John M Calhoun <johnmcalhoun123@gmail.com>
2018-12-13 09:42:11 -0600

    add user login information from cognito to dashboard

--- templates/main/api/lambda/User/Get.js
+++ templates/main/api/lambda/User/Get.js
@@ -1,5 +1,6 @@
 var aws=require('aws-sdk')
 aws.config.region=process.env.AWS_REGION
+var cognito=new aws.CognitoIdentityServiceProvider()
 var lambda=new aws.Lambda()
 
 exports.handler=function(event,context,callback){
@@ -10,29 +11,38 @@ exports.handler=function(event,context,callback){
         Payload:JSON.stringify(event)
     }).promise().then(validate)
     .then(result=>{
-        lambda.invoke({
-            FunctionName:process.env.ESPROXY,
-            InvocationType:"RequestResponse",
-            Payload:JSON.stringify({
-                 "endpoint":process.env.ESADDRESS,
-                 "path":"/logins-*/_search",
-                 "method":"GET",
-                 "body":{
-                     "size":4,
-                     "query":{
-                         "term":{
-                             "UserName":result.attributes.ID
-                         }
-                     },
-                     "sort":[{
-                        "Date":{
-                            "order":"desc"
-                        }
-                     }]
-                 }  
-            })
-        }).promise().then(validate)
-        .then(es=>{
+        Promise.all([
+            lambda.invoke({
+                FunctionName:process.env.ESPROXY,
+                InvocationType:"RequestResponse",
+                Payload:JSON.stringify({
+                     "endpoint":process.env.ESADDRESS,
+                     "path":"/logins-*/_search",
+                     "method":"GET",
+                     "body":{
+                         "size":4,
+                         "query":{
+                             "term":{
+                                 "UserName":result.attributes.ID
+                             }
+                         },
+                         "sort":[{
+                            "Date":{
+                                "order":"desc"
+                            }
+                         }]
+                     }  
+                })
+            }).promise().then(validate),
+            cognito.adminListUserAuthEvents({
+                UserPoolId:event.UserPool,
+                Username:result.attributes.ID,
+                MaxResults:4
+            }).promise()
+        ])
+        .then(results=>{
+            var es=results[0]
+            var cog=results[1]
             if(es.hits.total>0){
                 return Promise.all(es.hits.hits.map(x=>lambda.invoke({
                     FunctionName:event.FunctionName,
@@ -44,6 +54,10 @@ exports.handler=function(event,context,callback){
                 }).promise().then(validate)
                 .then(y=>`${y.attributes.ID} ${x._source.Date}`)))
                 .then(x=>result.attributes["Last Logins"]=x)
+            }
+            if(cog.AuthEvents.length>0){
+                result.attributes["Login events"]=cog.AuthEvents
+                    .map(x=>`${x.CreationDate}:${x.EventType} ${x.EventResponse}:${x.EventRisk.RiskLevel || ""}`)
             }
         })
         .then(()=>{
